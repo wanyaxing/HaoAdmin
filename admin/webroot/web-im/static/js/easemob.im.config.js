067cd784bf74e7ecd67ff41fae30fdaf8af51c1e
wanyaxing <wyx@wanyaxing.com>
2016-07-25 17:36:51 +0800

    + 后台页面默认使用环信接口进行推送和通知消息。

---
+++ admin/webroot/web-im/static/js/easemob.im.config.js
@@ -0,0 +1,27 @@
+Easemob.im.config = (function () {
+    var protocol = location.protocol === 'https:' ? 'https:' : 'http:';
+
+    return {
+        /*
+            The global value set for xmpp server
+        */
+        xmppURL: 'im-api.easemob.com',
+        /*
+            The global value set for Easemob backend REST API
+            "http://a1.easemob.com"
+        */
+        apiURL: protocol + '//a1.easemob.com',
+        /*
+            连接时提供appkey
+        */
+        appkey: "haoxitech#haoframe",
+        /*
+         * 是否使用https
+         */
+        https : protocol === 'https:',
+        /*
+         * 是否使用多resource
+         */
+        multiResources: true
+    };
+}());
