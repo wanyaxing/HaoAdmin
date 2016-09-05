98f9181a8fe030c3947adb4c9cdc2a82118a0ba8
wanyaxing <wyx@wanyaxing.com>
2016-09-05 17:23:08 +0800

    * 优化头部导航条显示

--- admin/elo/home.php
+++ admin/elo/home.php
@@ -16,34 +16,34 @@
 </html>
 <?php else: ?>
 <body>
-    <nav class="navbar navbar-default navbar-fixed-top">
-      <div id="home_navcontainer" class="container-fluid">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#haoadmin-navbar-collapse-1" aria-expanded="false">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="/">
-            <?= AXAPI_PROJECT_TITLE ?>
-          </a>
-        </div>
-        <div class="collapse navbar-collapse" id="haoadmin-navbar-collapse-1">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-              <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button"><span class="glyphicon glyphicon-user"></span><?= Utility::getCurrentUserName() ?><span class="caret"></span></a>
-              <ul class="dropdown-menu">
-                <li><a href=/edit/user_detail?id=<?= Utility::getCurrentUserId() ?> >个人中心</a></li>
-                <li role="separator" class="divider"></li>
-                <li><a href="javascript:;" onclick="HaoAdmin.user_login()" >退出</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
     <div class="container" id="home_container">
+      <nav class="navbar navbar-default">
+        <div id="home_navcontainer" class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#haoadmin-navbar-collapse-1" aria-expanded="false">
+              <span class="sr-only">Toggle navigation</span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="/">
+              <?= AXAPI_PROJECT_TITLE ?>
+            </a>
+          </div>
+          <div class="collapse navbar-collapse" id="haoadmin-navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+              <li class="dropdown">
+                <a href="javascript:;" class="dropdown-toggle" data-toggle="dropdown" role="button"><span class="glyphicon glyphicon-user"></span><?= Utility::getCurrentUserName() ?><span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href=/edit/user_detail?id=<?= Utility::getCurrentUserId() ?> >个人中心</a></li>
+                  <li role="separator" class="divider"></li>
+                  <li><a href="javascript:;" onclick="HaoAdmin.user_login()" >退出</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
       <div class="row" id="div_alert_notice">
       </div>
       <div class="row">
