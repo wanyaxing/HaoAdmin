42a3af6bd78b1bdc68a58f87c3418ec73cd722d4
wanyaxing <wyx@wanyaxing.com>
2018-05-11 10:33:11 +0800

    * no more ie

--- admin/elo/ie_no_more.php
+++ admin/elo/ie_no_more.php
@@ -1,9 +1,11 @@
 <?php
     header('HTTP/1.0 500 Internal Server Error');
 ?>
-<html>
+<!DOCTYPE html>
+<html lang="zh-CN">
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf8"/>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;"/>
     <title>为了更好的体验，请使用更先进的浏览器。</title>
 </head>
